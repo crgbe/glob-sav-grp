f91d543cf7f35b8cb894d429531532b82eb25125
Crédo Junior GBEDEDJI <cjgbededji@groupe-igs.fr>
2022-12-02 14:12:01 +0100

    Adding boostrap 5 to the project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,9 @@ import router from '@/global/router'
 import store from '@/global/store'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 createApp(App)
 	.use(router)
 	.use(store)
